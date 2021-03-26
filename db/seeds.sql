1a952cf79f0493e1c49adb9ec2ef0b76593322c2
Karl Tulio <kltulio@up.edu.ph>
2021-03-25 21:51:16 -0400

    2nd commit
    connection.js

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
+
+INSERT INTO burgers (burger_name) VALUES ('Le Big Mac');
+
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
